309e0341ba219613ba505e8a277338279ac0c339
Tyler West <tyler@TylerPC.localdomain>
2021-03-14 17:32:48 -0400

    eh

--- ftp_client.py
+++ ftp_client.py
@@ -1,18 +1,30 @@
 #ftp_client
 import socket
+import sys
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
-status = 1;
-while(status == 1):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-        val = input("Send or quit?")
-        if(val == "Send" or val == "s"):
-            s.sendall(b'1')
-            data = s.recv(1024)
-        if(val == "Quit" or val == "q"):
-            status = 0
+status = 1
 
-print('Received', repr(data))
+def connect(argv):
+    Host = arg1
+    PORT = arg2
+
+
+#while(status == 1):
+
+
+    #HOST = '127.0.0.1'  # The server's hostname or IP address
+    #PORT = 65432        # The port used by the server
+
+#    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#        s.connect((HOST, PORT))
+#        val = input("Send or quit?")
+#        if(val == "Send" or val == "s"):
+#            s.sendall(b'1')
+#            data = s.recv(1024)
+#        if(val == "Quit" or val == "q"):
+#            status = 0
+#
+#    print('Received', repr(data))
